904c1a518fd15e2bf69b90dec523394b468c64c6
Adem Emre <ademeg_42@hotmail.com>
2022-06-04 15:40:59 +0300

    Authentication Value Objects

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_example/domain/core/value_objects.dart';
+
+import '../core/failures.dart';
+import '../core/value_validators.dart';
+
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  const EmailAddress._(
+    this.value,
+  );
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(validateEmailAddress(input));
+  }
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  const Password._(
+    this.value,
+  );
+
+  factory Password(String input) {
+    return Password._(validatePassword(input));
+  }
+}
