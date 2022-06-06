1e5448bed20be8ea72648b27e4df4208a503f2f8
Adem Emre <ademeg_42@hotmail.com>
2022-06-06 13:33:14 +0300

    Sign-In Form Logic (Bloc)

--- lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:ddd_example/domain/auth/auth_failure.dart';
+import 'package:ddd_example/domain/auth/i_auth_facade.dart';
 import 'package:ddd_example/domain/auth/value_objects.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -9,7 +10,63 @@ part 'sign_in_form_state.dart';
 part 'sign_in_form_bloc.freezed.dart';
 
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
-  SignInFormBloc() : super(SignInFormState.initial()) {
-    on<SignInFormEvent>((event, emit) {});
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial()) {
+    on<SignInFormEvent>((event, emit) {
+      void _performActionOnAuthFacadeWithEmailAndPassword(
+          Future<Either<AuthFailure, Unit>> Function(
+                  {required EmailAddress email, required Password password})
+              forwardedCall) async {
+        final isEmailValid = state.emailAddress.isValid();
+        final isPasswordValid = state.password.isValid();
+        Either<AuthFailure, Unit>? failureOrSuccess;
+        if (isEmailValid && isPasswordValid) {
+          emit(state.copyWith(
+            isSubmitting: true,
+            authFailureOrSuccessOption: none(),
+          ));
+          failureOrSuccess = await forwardedCall(
+            email: state.emailAddress,
+            password: state.password,
+          );
+        }
+
+        emit(state.copyWith(
+          isSubmitting: false,
+          showErrorMessages: true,
+          authFailureOrSuccessOption: optionOf(failureOrSuccess),
+        ));
+      }
+
+      event.map(
+          emailChanged: (e) => emit(state.copyWith(
+                emailAddress: EmailAddress(e.emailStr),
+                authFailureOrSuccessOption: none(),
+              )),
+          passwordChanged: (e) => emit(state.copyWith(
+                password: Password(e.passwordStr),
+                authFailureOrSuccessOption: none(),
+              )),
+          registerWithEmailAndPasswordPressed: (e) async {
+            _performActionOnAuthFacadeWithEmailAndPassword(
+                _authFacade.registerWithEmailAndPassword);
+          },
+          signInWithEmailAndPasswordPressed: (e) {
+            _performActionOnAuthFacadeWithEmailAndPassword(
+                _authFacade.signInWithEmailAndPassword);
+          },
+          signInWithGooglePressed: (e) async {
+            emit(state.copyWith(
+              isSubmitting: true,
+              authFailureOrSuccessOption: none(),
+            ));
+            final failureOrSuccess = await _authFacade.signInWithGoogle();
+            emit(state.copyWith(
+              isSubmitting: false,
+              authFailureOrSuccessOption: some(failureOrSuccess),
+            ));
+          });
+    });
   }
 }
--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,14 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:ddd_example/domain/auth/auth_failure.dart';
+import 'package:ddd_example/domain/auth/value_objects.dart';
 
 abstract class IAuthFacade{
   Future<Either<AuthFailure,Unit>> registerWithEmailAndPassword({
-    required String email,
-    required String password,
+    required EmailAddress email,
+    required Password password,
   });
   Future<Either<AuthFailure,Unit>> signInWithEmailAndPassword({
-    required String email,
-    required String password,
+    required EmailAddress email,
+    required Password password,
   });
   Future<Either<AuthFailure,Unit>> signInWithGoogle();
 }
